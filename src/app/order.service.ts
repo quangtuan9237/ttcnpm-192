89062cfe36fbbdcbdff2105e880aaa0422e252dc
Nguyet <nguyetdoan1010@gmail.com>
2020-07-15 23:31:51 +0700

    need check view order

--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -1,7 +1,8 @@
 import { ShoppingCartService } from './shopping-cart.service';
-import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireDatabase, SnapshotAction } from '@angular/fire/database';
 import { Injectable } from '@angular/core';
 import { map, switchMap, zip } from 'rxjs/operators';
+import { domainToUnicode } from 'url';
 
 @Injectable({
   providedIn: 'root'
@@ -24,14 +25,28 @@ export class OrderService {
   // }
 
   getOrdersByUser(userId: string) {
+    // cai nay return 1 list, list ko co id nua roi :) gio chi muon co id :)
+    // co nen query nhu nay? idk :/ cung duoc :/
     return this.db.list('/orders', query => query.orderByChild('userId').equalTo(userId))
-    .valueChanges().pipe(
-      map(test => {
-        console.log(test);
-        return test
+    // .valueChanges().pipe( // chi xai value change no se ko co id neu' la list ok?uhm
+    //   map(test => {
+    //     console.log(test);
+    //     return test
+    //   })
+    // )
+
+    .snapshotChanges().pipe( // trong cai nay no' co key ne
+      map(snapshot => {
+        return snapshot.map(snap => {
+          let val = snap.payload.val();
+          val['key'] = snap.key;
+          return val
+        })
       })
     )
   }
+  // chi sua gi r cai nay chi them vao ma em co lam ham nay dau ok :)
+  // uas thi noi binh thuong thoi an noi kieu gi vay
 
   // getOrdersByUser(user_id){
   //   return this.db.object(`/orders`).valueChanges().pipe(
@@ -45,6 +60,7 @@ export class OrderService {
 
 
   getAll(){
+    // khac nhau nha :)  cai nay return 1 dictionary
     return this.db.object('/orders').valueChanges().pipe(
       map(test => {
         // console.log(test);
